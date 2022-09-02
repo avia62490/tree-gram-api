e423abe9dda81b6c859dd3e8a2e7d5fac86d2939
Alex Via <alexandervia@gmail.com>
2022-09-02 13:51:01 -0500

    adds Follow model, adds likes(integer) to Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_02_173801) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_02_184758) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_02_173801) do
     t.text "body"
     t.integer "post_id"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -30,6 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_02_173801) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "likes"
   end
 
   create_table "users", force: :cascade do |t|
